0cbd0d4dfb879d082003da62531c8434930b21c3
fnikitin <frederic.nikitin@gmail.com>
2015-01-26 18:37:44 +0100

    new method that calculates dev version

--- create-release.sh
+++ create-release.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+RELEASE_VERSION=${1}
+
 # start debug mode
 set -x
 
@@ -7,30 +9,39 @@ stopDebugModeAndExit () {
   set +x; exit $1
 }
 
+calcNextVersion () {
+
+  # release version $1=x.y.z
+  xyz=$1
+  # 1. extract z from x.y.z
+  xy=${xyz%.*}
+  z=${xyz##*.}
+
+  # 2. increment z
+  zplus1=$((z+1))
+
+  # 3. construct and set dev version: x.y.z+1 to the 2nd argument
+  eval "$2=$xy.$zplus1-SNAPSHOT"
+}
+
 if [ -z "$1" ]; then
     echo "Specify release version as first argument"
     stopDebugModeAndExit 0
 fi
 
-if [ -z "$2" ]; then
-    echo "Specify development version as second argument"
-    stopDebugModeAndExit 0
-fi
-
-RELEASEVERSION=${1}
-DEVELOPMENTVERSION=${2}-SNAPSHOT
+calcNextVersion RELEASE_VERSION DEVELOPMENTVERSION
 
 # change to branch master
 git checkout master || stopDebugModeAndExit 1
 git merge develop || stopDebugModeAndExit 2
 
 # set new version in pom.xml
-mvn versions:set -DnewVersion=$RELEASEVERSION -DgenerateBackupPoms=false || stopDebugModeAndExit 3
+mvn versions:set -DnewVersion=$RELEASE_VERSION -DgenerateBackupPoms=false || stopDebugModeAndExit 3
 git add pom.xml
-git commit -m "New release version $RELEASEVERSION" # returns -1 if nothing to commit || stopDebugModeAndExit 4
+git commit -m "New release version $RELEASE_VERSION" # returns -1 if nothing to commit || stopDebugModeAndExit 4
 git push origin master || stopDebugModeAndExit 5
 # tag
-git tag -a v$RELEASEVERSION -m "tag v$RELEASEVERSION" || stopDebugModeAndExit 6
+git tag -a v$RELEASE_VERSION -m "tag v$RELEASE_VERSION" || stopDebugModeAndExit 6
 git push --tags || stopDebugModeAndExit 7
 # deploy on nexus
 mvn clean deploy || stopDebugModeAndExit 8
