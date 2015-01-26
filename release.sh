87d21f76c0df01fea65dfe628d40e81e3e04a00c
fnikitin <frederic.nikitin@gmail.com>
2015-01-26 16:44:40 +0100

    fixing pom.xml, adding exits when one of script command fails

--- release.sh
+++ release.sh
@@ -18,21 +18,28 @@ DEVELOPMENTVERSION=${2}-SNAPSHOT
 
 echo "Releasing with version: $RELEASEVERSION"
 
-git merge develop master
-git checkout master
-mvn versions:set -DnewVersion=$RELEASEVERSION
+git merge develop master || exit 1
+
+
+# change to branch master
+git checkout master || exit 2
+# set new version in pom.xml
+mvn versions:set -DnewVersion=$RELEASEVERSION || exit 3
 git add .
-git commit -m "Release version $RELEASEVERSION"
-git push origin master
-git tag -a v$RELEASEVERSION -m "tag v$RELEASEVERSION"
-git push --tags
-mvn clean deploy
-# change to develop 
-git checkout develop
-mvn versions:set -DnewVersion=$DEVELOPMENTVERSION
-git add pom.xml
-git commit -m "Setting new snapshot version $DEVELOPMENTVERSION"
-git push origin develop
+git commit -m "Release version $RELEASEVERSION" || exit 4
+git push origin master || exit 5
+# tag
+git tag -a v$RELEASEVERSION -m "tag v$RELEASEVERSION" || exit 5
+git push --tags || exit 6
+# deploy on nexus
+mvn clean deploy || exit 7
+
+# change to branch develop
+git checkout develop || exit 8
+mvn versions:set -DnewVersion=$DEVELOPMENTVERSION || exit 9
+git add pom.xml || exit 10
+git commit -m "Setting new snapshot version $DEVELOPMENTVERSION" || exit 11
+git push origin develop || exit 12
 
 
 
